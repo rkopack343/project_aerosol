07c0877f235e4144c09239c71002b4289166d386
Robert Kopack <Senkan343@gmail.com>
2021-03-13 21:23:25 -0500

    Cleaning up old files. I can just tell Eloquent to auto cast the JSON column in and out of JSON for me. The AeroHelper class is probably useless.

--- app/Models/Aerosol/Aero.php
+++ app/Models/Aerosol/Aero.php
@@ -5,9 +5,13 @@ use Aerosol\Helper\Image as Image;
 use Illuminate\Database\Eloquent\Model;
 
 class Aero extends Model implements \JsonSerializable {
-		protected $table = 'aero';
+	protected $table = 'aero';
 
-		protected $primaryKey = 'key';
-		protected $keyType ='string';
-		public $incrementing = 'false';
+	protected $primaryKey = 'key';
+	protected $keyType ='string';
+	public $incrementing = 'false';
+
+	protected $casts = [
+		'data' => 'array'
+	];
 }
